cab90b5e56bd05138fd688d5f3b83b4d5fa3dc57
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-27 13:39:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const input = document.getElementsByTagName('body');
+let index = 0;
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  input.addEventListener('keydown', function(e) {
+    while (index < code.length) {
+      if (e.which || e.detail === code[index]) {
+        index++;
+      } else {
+        index = 0;
+      }
+    }
+  });
 }
